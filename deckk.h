ba961e5dd201d02e4ce25b95faa6300535cea680
Iliyana Kastamanova <72686714+kastamanova@users.noreply.github.com>
2021-04-18 19:44:05 +0300

    Update deckk.h

--- deckk.h
+++ deckk.h
@@ -1,20 +1,23 @@
-#pragma once
+#ifndef DECK_H
+#define DECK_H
 
 class Deck
 {
 private:
 	char series[10];
+	Card cards[52];
 
 public:
 	void sequence();
 	Deck();
 	Deck(int k,char serieS[10]); //k-number of cards 
+	~Deck();
+	void random();
 	void draw();
 	void swap();
 	void suit_cout();
 	void rank_count();
 
 };
-
-
+#endif
  
